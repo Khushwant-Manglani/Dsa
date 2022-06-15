5cc629cfa08b56bfc58f9db61af6ec15bdc42ad1
Khushwant Manglani <107555256+Khushwant-Manglani@users.noreply.github.com>
2022-06-16 00:02:44 +0530

    Create Power of Two.cpp

---
+++ Bit Manipulation/Power of Two.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        /*if(n <= 0) return false;
+        int res = log2(n);
+        if(pow(2,res) == n)
+            return true;
+        return false;*/
+        
+        return (n > 0 && (n & n-1) == 0);
+        
+        //naive soln
+        /*int ans = 1;
+        for(int i=0; i<=30 ; i++){
+            if(ans == n){
+                return true;
+            }
+            if(ans < INT_MAX/2)
+            ans *= 2;
+            
+        }
+        return false;*/
+    }
+};
