b1dd3cc890aeac7a4e7f5061bb90ec91ef903320
Khushwant Manglani <107555256+Khushwant-Manglani@users.noreply.github.com>
2022-06-18 18:31:41 +0530

    Update Single Number III.cpp

--- Bit Manipulation/Single Number III.cpp
+++ Bit Manipulation/Single Number III.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        unsigned int single=0;
-        int res1=0,res2=0;
-        for(int x : nums) single ^= x;
-        single &= ~(single - 1);  //or we can use single &= -single;
+        
+        unsigned int single=0; 
+	    //If we take int variable then runtime error occur becoz of negation of -2147483648 cannot be int so therefore we take variable as unsigned int 
+	
+	    int res1=0,res2=0;  //Taking two variable to take the result of both half after xor the elements of both half
+	
+	    for(int x : nums) single ^= x; //Taking XOR of all the elements of nums
+	
+	    single &= ~(single - 1);  //or we can use single &= -single; also 
+	    //this statement is to find first set bit from right most side 
+	
         for(int x : nums){
-            if(x & single) res1 ^= x;
-            else res2 ^= x;
+            if(x & single) res1 ^= x; //This is for the first half of elements with not set bit 
+            else res2 ^= x; //This is for the second half of elements with set bit
         }
-        return {res1,res2};
+
+        return {res1,res2}; //This is our final result
         
         /*int res = 0,res1=0,res2=0;
         for(auto x : nums) res ^= x;
