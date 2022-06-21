8d532a2d7ddcf145418e0ffb0b2949075ed3ab86
Khushwant Manglani <107555256+Khushwant-Manglani@users.noreply.github.com>
2022-06-21 11:31:43 +0530

    Update Reverse Bits.cpp

--- Bit Manipulation/Reverse Bits.cpp
+++ Bit Manipulation/Reverse Bits.cpp
@@ -1,7 +1,25 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        unsigned long int mask = 0;
+        int mask = 0;
+        for(int i=0 ; i < 32 ; ++i){
+            if(n & 1 == 1)  mask = (mask << 1) | 1;
+            else mask <<= 1;
+            n >>= 1;
+        }
+        return mask;
+        
+        
+        /*int mask = (1 << 31),res = 0;
+        for(int i=0 ; i < 33 ; ++i){
+            if((mask & n) != 0)
+                res += (1 << i) * 1;
+            n <<= 1;
+        }
+        return res;*/
+        
+        
+        /*unsigned long int mask = 0;
         for(int i=0; i<=31 ;i++){
             if((n & 1) == 1){
                 mask |= 1;
@@ -12,7 +30,7 @@ public:
             }
             n >>= 1;
         }
-        return mask >> 1;
+        return mask >> 1;*/
         
         /*if(n == 0) return 0;
         int result = 0;
