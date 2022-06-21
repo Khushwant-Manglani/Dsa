b2e92f6ead29c502b92fb56813868e6faa6fa1ab
Khushwant Manglani <107555256+Khushwant-Manglani@users.noreply.github.com>
2022-06-21 13:26:44 +0530

    Update Find the Difference.cpp

--- Bit Manipulation/Find the Difference.cpp
+++ Bit Manipulation/Find the Difference.cpp
@@ -5,16 +5,14 @@ public:
             t[i+1] += t[i] - s[i]; //we'll put the diff of t[i] and s[i] in t[i+1]
         }
         return t[t.size() - 1]; // So at last diff will com in last element it is ans
-       
         
-        /*int n = s.length();
-        int m = t.length();
-        int res = 0;
-        for(int i=0,j=0 ; i<n || j<m ; i++,j++){
-            if(i < n) res ^= s[i];
-            if(j < m) res ^= t[i];
+        
+        /*int res = t[0];
+        for(int i=0 ; i < s.length() ; i++){
+            res ^= s[i] ^ t[i+1];
         }
         return res;*/
+        
         
         /*unordered_map<int,int> freq;
         for(auto x : s) freq[x]++; 
