9e97074644d6fdaf5d8f33ed8b703e03f4150683
Khushwant Manglani <107555256+Khushwant-Manglani@users.noreply.github.com>
2022-06-21 11:34:56 +0530

    Update Number of 1 Bits.cpp

--- Bit Manipulation/Number of 1 Bits.cpp
+++ Bit Manipulation/Number of 1 Bits.cpp
@@ -28,5 +28,22 @@ public:
             n = n / 2;
         }
         return count;*/
+        
+        /*int Co = 0;
+        for(int i=0 ; i<32 ; ++i){
+            if(((1 << i) & n) != 0)
+                Co++;
+        }
+        return Co;*/
+        
+        
+        /*int mask = 0,Co = 0;
+        for(int i=0 ; i<32 ; i++){
+            mask |= 1;
+            mask &= n;
+            if(mask == 1) Co++;
+            n >>= 1;
+        }
+        return Co;*/
     }
 };
