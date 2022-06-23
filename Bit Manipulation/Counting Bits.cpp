12e0ad0f25ff117bd32ba788a811ca481dfe2bbc
Khushwant Manglani <107555256+Khushwant-Manglani@users.noreply.github.com>
2022-06-23 08:16:35 +0530

    Update Counting Bits.cpp

--- Bit Manipulation/Counting Bits.cpp
+++ Bit Manipulation/Counting Bits.cpp
@@ -1,3 +1,55 @@
+//DP method- explaination
+Index : 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+
+num : 0 1 1 2 1 2 2 3 1 2 2 3 2 3 3 4
+
+Do you find the pattern?
+
+Obviously, this is overlap sub problem, and we can come up the DP solution. For now, we need find the function to implement DP.
+
+dp[0] = 0;
+
+dp[1] = dp[0] + 1;
+
+dp[2] = dp[0] + 1;
+
+dp[3] = dp[1] +1;
+
+dp[4] = dp[0] + 1;
+
+dp[5] = dp[1] + 1;
+
+dp[6] = dp[2] + 1;
+
+dp[7] = dp[3] + 1;
+
+dp[8] = dp[0] + 1;
+...
+
+This is the function we get, now we need find the other pattern for the function to get the general function. After we analyze the above function, we can get
+dp[0] = 0;
+
+dp[1] = dp[1-1] + 1;
+
+dp[2] = dp[2-2] + 1;
+
+dp[3] = dp[3-2] +1;
+
+dp[4] = dp[4-4] + 1;
+
+dp[5] = dp[5-4] + 1;
+
+dp[6] = dp[6-4] + 1;
+
+dp[7] = dp[7-4] + 1;
+
+dp[8] = dp[8-8] + 1;
+..
+
+Obviously, we can find the pattern for above example, so now we get the general function
+
+dp[index] = dp[index - offset] + 1;
+
 class Solution {
 public:
     vector<int> countBits(int n) {
